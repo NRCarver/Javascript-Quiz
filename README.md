401259628ec8ae73334a4f07b8e228dbf7790c38
NRCarver <carver1994@hotmail.com>
2022-06-28 16:39:00 +0000

    Add JS outline

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 https://pixabay.com/illustrations/question-quiz-think-thinking-2004314/ - quiz pic
 freelogodesign.org - logo generator
+https://www.pexels.com/photo/pills-fixed-as-question-mark-sign-3683053/ - question mark background
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
-console.log("Hello World!")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("buttons");
+
+    for (let buttons of buttons) {
+        buttons.addEventListener("click", function() {
+            if (this === "answer-buttons" {
+                alert("You clicked submit!");
+            })
+
+        })
+    }
+})
+
+
+function runGame () {
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculateAnswer () {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayQuestion () {
+
+}
+
+function addToLeaderboard () {
+    
+}
